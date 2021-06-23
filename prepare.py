c035b86964c385a1ac19fb54fc5510b19d041f68
lupeluna <80718680+lupeluna@users.noreply.github.com>
2021-06-23 17:17:08 -0500

    new functions

---
+++ prepare.py
@@ -0,0 +1,156 @@
+import pandas as pd
+import numpy as np
+
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.model_selection import train_test_split
+
+
+
+def handle_missing_values(df, prop_required_column = .7, prop_required_row = .7):
+    ''' 
+        take in a dataframe and a proportion for columns and rows
+        return dataframe with columns and rows not meeting proportions dropped
+    '''
+    col_thresh = int(round(prop_required_column*df.shape[0],0)) # calc column threshold
+    
+    df.dropna(axis=1, thresh=col_thresh, inplace=True) # drop columns with non-nulls less than threshold
+    
+    row_thresh = int(round(prop_required_row*df.shape[1],0))  # calc row threshhold
+    
+    df.dropna(axis=0, thresh=row_thresh, inplace=True) # drop columns with non-nulls less than threshold
+    
+    return df
+
+
+
+
+# def split_continuous(df):
+#     """
+#     Takes in a df
+#     Returns train, validate, and test DataFrames
+#     """
+#     # Create train_validate and test datasets
+#     train_validate, test = train_test_split(df, test_size=0.2, random_state=123)
+#     # Create train and validate datsets
+#     train, validate = train_test_split(train_validate, test_size=0.3, random_state=123)
+
+#     # Take a look at your split datasets
+
+#     print(f"train -> {train.shape}")
+#     print(f"validate -> {validate.shape}")
+#     print(f"test -> {test.shape}")
+#     return train, validate, test
+
+
+
+def train_validate_test(df, target):
+    """
+    this function takes in a dataframe and splits it into 3 samples,
+    a test, which is 20% of the entire dataframe,
+    a validate, which is 24% of the entire dataframe,
+    and a train, which is 56% of the entire dataframe.
+    It then splits each of the 3 samples into a dataframe with independent variables
+    and a series with the dependent, or target variable.
+    The function returns 3 dataframes and 3 series:
+    X_train (df) & y_train (series), X_validate & y_validate, X_test & y_test.
+    """
+    # split df into test (20%) and train_validate (80%)
+    train_validate, test = train_test_split(df, test_size=0.2, random_state=123)
+
+    # split train_validate off into train (70% of 80% = 56%) and validate (30% of 80% = 24%)
+    train, validate = train_test_split(train_validate, test_size=0.3, random_state=123)
+
+    # split train into X (dataframe, drop target) & y (series, keep target only)
+    X_train = train.drop(columns=[target])
+    y_train = train[target]
+
+    # split validate into X (dataframe, drop target) & y (series, keep target only)
+    X_validate = validate.drop(columns=[target])
+    y_validate = validate[target]
+
+    # split test into X (dataframe, drop target) & y (series, keep target only)
+    X_test = test.drop(columns=[target])
+    y_test = test[target]
+
+    return X_train, y_train, X_validate, y_validate, X_test, y_test
+
+
+
+
+
+def detect_outliers(df, k, col_list):
+    ''' get upper and lower bound for list of columns in a dataframe 
+        if desired return that dataframe with the outliers removed
+    '''
+    
+    odf = pd.DataFrame()
+    
+    for col in col_list:
+
+        q1, q2, q3 = df[f'{col}'].quantile([.25, .5, .75])  # get quartiles
+        
+        iqr = q3 - q1   # calculate interquartile range
+        
+        upper_bound = q3 + k * iqr   # get upper bound
+        lower_bound = q1 - k * iqr   # get lower bound
+        
+        # print each col and upper and lower bound for each column
+        print(f"{col}: Median = {q2} lower_bound = {lower_bound} upper_bound = {upper_bound}")
+
+        # return dataframe of outliers
+        odf = odf.append(df[(df[f'{col}'] < lower_bound) | (df[f'{col}'] > upper_bound)])
+            
+    return odf
+
+
+
+
+
+
+def create_features(df):
+    df['age'] = 2017 - df.yearbuilt
+    df['age_bin'] = pd.cut(df.age, 
+                           bins = [0, 5, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 110, 120, 130, 140],
+                           labels = [0, .066, .133, .20, .266, .333, .40, .466, .533, 
+                                     .60, .666, .733, .8, .866, .933])
+
+    # create taxrate variable
+    df['taxrate'] = df.taxamount/df.taxvaluedollarcnt*100
+
+    # create acres variable
+    df['acres'] = df.lotsizesquarefeet/43560
+
+    # bin acres
+    df['acres_bin'] = pd.cut(df.acres, bins = [0, .10, .15, .25, .5, 1, 5, 10, 20, 50, 200], 
+                       labels = [0, .1, .2, .3, .4, .5, .6, .7, .8, .9])
+
+    # square feet bin
+    df['sqft_bin'] = pd.cut(df.calculatedfinishedsquarefeet, 
+                            bins = [0, 800, 1000, 1250, 1500, 2000, 2500, 3000, 4000, 7000, 12000],
+                            labels = [0, .1, .2, .3, .4, .5, .6, .7, .8, .9]
+                       )
+    
+    # dollar per square foot-structure
+    df['structure_dollar_per_sqft'] = df.structuretaxvaluedollarcnt/df.calculatedfinishedsquarefeet
+
+    df['structure_dollar_sqft_bin'] = pd.cut(df.structure_dollar_per_sqft, 
+                                             bins = [0, 25, 50, 75, 100, 150, 200, 300, 500, 1000, 1500],
+                                             labels = [0, .1, .2, .3, .4, .5, .6, .7, .8, .9]
+                                            )
+
+    # dollar per square foot-land
+    df['land_dollar_per_sqft'] = df.landtaxvaluedollarcnt/df.lotsizesquarefeet
+
+    df['lot_dollar_sqft_bin'] = pd.cut(df.land_dollar_per_sqft, bins = [0, 1, 5, 20, 50, 100, 250, 500, 1000, 1500, 2000],
+                                       labels = [0, .1, .2, .3, .4, .5, .6, .7, .8, .9]
+                                      )
+
+    # update datatypes of binned values to be float
+    df = df.astype({'sqft_bin': 'float64', 'acres_bin': 'float64', 'age_bin': 'float64',
+                    'structure_dollar_sqft_bin': 'float64', 'lot_dollar_sqft_bin': 'float64'})
+
+    # ratio of bathrooms to bedrooms
+    df['bath_bed_ratio'] = df.bathroomcnt/df.bedroomcnt
+
+    return df
